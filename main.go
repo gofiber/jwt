3e649e579dcf967a163562f3e5ceb4495fe72a2f
Fenny <25108519+Fenny@users.noreply.github.com>
2020-03-28 07:59:03 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 // 🚀 Fiber is an Express inspired web framework written in Go with 💖
 // 📌 API Documentation: https://fiber.wiki
 // 📝 Github Repository: https://github.com/gofiber/fiber
+// Special thanks to Echo: https://github.com/labstack/echo/blob/master/middleware/jwt.go
 
 package jwt
 

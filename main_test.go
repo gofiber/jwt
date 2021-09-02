7e0986c954032797b75935601e758aafa5ade50b
Alexey Shytikov <alexey.shytikov@gmail.com>
2021-09-02 16:19:51 +0200

    #48: adding tests for `initExtractors` function

--- main_test.go
+++ main_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+var defaultTokenLookup = "header:" + fiber.HeaderAuthorization
+
 func TestPanicOnMissingConfiguration(t *testing.T) {
 	defer func() {
 		// Assert
@@ -48,12 +50,33 @@ func TestDefaultConfiguration(t *testing.T) {
 		t.Fatalf("Default claims should not be 'nil'")
 	}
 
-	defaultTokenLookup := "header:" + fiber.HeaderAuthorization
-
 	if cfg.TokenLookup != defaultTokenLookup {
 		t.Fatalf("Default token lookup should be '%v'", defaultTokenLookup)
 	}
 	if cfg.AuthScheme != "Bearer" {
 		t.Fatalf("Default auth scheme should be 'Bearer'")
+	}
+}
+
+func TestExtractorsInitialization(t *testing.T) {
+	defer func() {
+		// Assert
+		if err := recover(); err != nil {
+			t.Fatalf("Middleware should not panic")
+		}
+	}()
+
+	// Arrange
+	cfg := Config{
+		SigningKey:  "",
+		TokenLookup: defaultTokenLookup + ",query:token,param:token,cookie:token,something:something",
+	}
+
+	// Act
+	extractors := initExtractors(cfg)
+
+	// Assert
+	if len(extractors) != 4 {
+		t.Fatalf("Extractors should not be created for invalid lookups")
 	}
 }
